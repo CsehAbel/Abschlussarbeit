f323f8dc98188e9609d8e026644a309826c4d695
ABEL <abel_7910@hotmail.com>
2019-11-23 17:20:18 +0100

    mindharomimplementacio

--- emts/main.py
+++ emts/main.py
@@ -2,7 +2,7 @@ import subprocess
 import re
 import numpy
 
-
+#
 def docker_to_file(in_file, out_file):
     task = subprocess.Popen("cat %s | docker run -i mtaril/emtsv tok,spell,morph,pos,conv-morph,dep,chunk,ner" % in_file, shell=True, stdout=subprocess.PIPE)
     with open('%s' % out_file, 'w') as f:
@@ -13,14 +13,13 @@ def docker_to_file(in_file, out_file):
 #             arrayNPint[.............................................]     arrayNPint[....]
 #                            np['stem','B-NP']...np['stem','E-NP']
 def generate_3d_array(out_file):
-    #Dokumentálni a feldarabolást vizuálisan
-    arrayNPext = []
+    arrayNPext = [] #Belső listái tartalmazzak a főnévi csoportokat
     with open('%s' % out_file) as f:
         sor = f.readline()
-        arrayNPint = []
-        while sor:
+        arrayNPint = [] #Egy darab főnévi csoportot tartalmaz
+        while sor: #A dokumentum e-magyar eszközlánc általi outputján soronként iterálunk végig
             patternStem = re.compile('^(.*?)\s.*"stem": \["([1-9a-zA-ZÁÉÍÓÖŐÚÜŰáéíóöőúüű]+?)".*\s([^\s]+?)\s[^\s]+$')
-            resultStem = patternStem.match(sor)
+            resultStem = patternStem.match(sor) #a reguláris kifejezés egyezőségét tartlamazű boolean
             patternLemma = re.compile('^(.*?)\s.*"lemma": "([1-9a-zA-ZÁÉÍÓÖŐÚÜŰáéíóöőúüű]+?)".*\s([^\s]+?)\s[^\s]+$')
             resultLemma = patternLemma.match(sor)
             patternRaw = re.compile('^([1-9a-zA-ZÁÉÍÓÖŐÚÜŰáéíóöőúüű\-]+?)\s.*\s([^\s]+?)\s[^\s]+$')
@@ -29,7 +28,8 @@ def generate_3d_array(out_file):
             resultPunct = patternPunct.match(sor)
             NP = ""
             sorArray = []
-            if resultStem:
+            if resultStem: #megvizsgáljuk, melyik reguláris kifejezés illik az adott sorra és a kívánt egyezőségi csoportot
+                           # azaz a szótövet és a főnévi csoportban elfoglalt helyet eltároljuk
                 sorArray.extend([resultStem.group(2), resultStem.group(3)])
                 NP = resultStem.group(3)
                 print('%s, %s' % (resultStem.group(1), resultStem.group(3)))
@@ -115,7 +115,7 @@ bow2 = generate_bow_matrix(generate_bow(generate_3d_array('emChunker.txt')), gen
 from numpy import dot
 from numpy.linalg import norm
 
-
+#compare bag_of_words
 def compare_bows(bow_1, bow_2):
     sum_of_max = 0
     for vector_b1 in bow_1:
@@ -129,9 +129,75 @@ def compare_bows(bow_1, bow_2):
         sum_of_max += max_cos_sim
     return sum_of_max
 
+#Teszt
 print(compare_bows(bow, bow1))
 print(compare_bows(bow, bow2))
 
+
+# Jaccard hasonlóság
+def intersection(lst1, lst2):
+    return list(set(lst1) & set(lst2))
+
+
+def union(lst1, lst2):
+    return list(set(lst1) | set(lst2))
+
+
+def generate__array(out_file):
+    with open('%s' % out_file) as f:
+        sor = f.readline()
+        arrayStem = []
+        while sor: #A dokumentum e-magyar eszközlánc általi outputján soronként iterálunk végig
+            patternStem = re.compile('^(.*?)\s.*"stem": \["([1-9a-zA-ZÁÉÍÓÖŐÚÜŰáéíóöőúüű]+?)".*\s([^\s]+?)\s[^\s]+$')
+            resultStem = patternStem.match(sor) #a reguláris kifejezés egyezőségét tartlamazű boolean
+            patternLemma = re.compile('^(.*?)\s.*"lemma": "([1-9a-zA-ZÁÉÍÓÖŐÚÜŰáéíóöőúüű]+?)".*\s([^\s]+?)\s[^\s]+$')
+            resultLemma = patternLemma.match(sor)
+            patternRaw = re.compile('^([1-9a-zA-ZÁÉÍÓÖŐÚÜŰáéíóöőúüű\-]+?)\s.*\s([^\s]+?)\s[^\s]+$')
+            resultRaw = patternRaw.match(sor)
+            if resultStem:
+                arrayStem.extend(resultStem.group(2))
+            elif resultLemma:
+                arrayStem.extend(resultLemma.group(2))
+            elif resultRaw:
+                arrayStem.extend(resultRaw.group(1))
+            else:
+                sor = f.readline()
+                continue
+            sor = f.readline()
+    return arrayStem
+
+
+def jaccard_sim(out_file1, out_file2):
+    i = intersection(generate__array(out_file1), generate__array(out_file2)).__len__()
+    u = union(generate__array(out_file1), generate__array(out_file2)).__len__()
+    return i/u
+
+
+print(jaccard_sim('emChunker.txt', 'emChunker1.txt'))
+print(jaccard_sim('emChunker1.txt', 'emChunker2.txt'))
+
+#Euklideszi távolság
+from scipy.spatial import distance
+p1 = (1, 2, 3)
+p2 = (4, 5, 6)
+d = distance.euclidean(p1, p2)
+
+def compare_bows_eucl(bow_1, bow_2):
+    sum_of_max = 0
+    for vector_b1 in bow_1:
+        max_cos_sim = 0
+        for vector_b2 in bow_2:
+            if numpy.count_nonzero(vector_b1) == 0 or numpy.count_nonzero(vector_b2) == 0:
+                continue
+            cos_sim = dot(vector_b1, vector_b2) / (norm(vector_b1) * norm(vector_b2))
+            if max_cos_sim < cos_sim:
+                max_cos_sim = cos_sim
+        sum_of_max += max_cos_sim
+    return sum_of_max
+
+#Teszt
+print(compare_bows_eucl(bow, bow1))
+print(compare_bows_eucl(bow, bow2))
 
 import urllib.request
 import bs4
